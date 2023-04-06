7acd739dd54053bd535d696ab8d67956c723c6d0
trevor dewindt <trevor.dewindt@gmail.com>
2023-04-05 23:02:42 -0600

    add model associations and seed data

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,4 @@
 class Plant < ApplicationRecord
+    has_many :forum_entries
+    has_many :users, through: :forum_entries
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 require 'bcrypt'
-User.create(username: "Mike", password_digest: BCrypt::Password.create('test'))
+User.create(username: "Jane", password_digest: BCrypt::Password.create('test'))
+User.create(username: "Mike", password_digest: BCrypt::Password.create('test2'))
+
+Plant.create(
+    name: "Rose",
+    image: "image.jpg",
+    light: "lots",
+    water: "1 cup a day",
+    size: "3 feet",
+    description: "Nice smell, red",
+    safe_for_pets: true
+)
+
+ForumEntry.create(user_id: 1, plant_id: 1, entry: "Love this plant!")
+ForumEntry.create(user_id: 2, plant_id: 1, entry: "Meh")
