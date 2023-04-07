27609ed3e6488b234128f7aecbfcba0afe23531a
trevor dewindt <trevor.dewindt@gmail.com>
2023-04-06 23:28:25 -0600

    create login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,20 @@
-function Login() {
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+export default function Login() {
     return (
-        <p>Login Page</p>
+        <div>
+            <Form>
+                <Form.Group>
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control placeholder="Enter username" name="username"/>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Enter password" name="password"/>
+                </Form.Group>
+                <Button variant="success" type="submit">Log in</Button>
+            </Form>
+        </div>
     )
 }
-
-export default Login
