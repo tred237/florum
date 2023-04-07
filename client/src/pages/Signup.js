4c3a923dbed8306f3c8614bf9b5c41a39e411741
trevor dewindt <trevor.dewindt@gmail.com>
2023-04-06 21:25:56 -0600

    make signup form controlled

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -4,19 +4,40 @@ import Button from 'react-bootstrap/Button';
 
 
 export default function Signup() {
+    const formDataDefault = {
+        username: '',
+        password: '',
+        passwordConfirmation: ''
+    }
+
+    const [formData, setFormData] = useState(formDataDefault)
+
+    const handleChange = (e) => setFormData({...formData, [e.target.name]: e.target.value})
+
     return (
         <Form>
             <Form.Group>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email"/>
+                <Form.Label>Username</Form.Label>
+                <Form.Control placeholder="Enter username"
+                              name="username"
+                              value={formData.username}
+                              onChange={handleChange} />
             </Form.Group>
             <Form.Group>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter password"/>
+                <Form.Control type="password"
+                              placeholder="Enter password"
+                              name="password"
+                              value={formData.password}
+                              onChange={handleChange} />
             </Form.Group>
             <Form.Group>
                 <Form.Label>Password Confirmation</Form.Label>
-                <Form.Control type="password" placeholder="Enter password confirmation"/>
+                <Form.Control type="password" 
+                              placeholder="Enter password confirmation"
+                              name="passwordConfirmation" 
+                              value={formData.passwordConfirmation} 
+                              onChange={handleChange} />
             </Form.Group>
             <Button variant="success" type="submit">Create Account</Button>
         </Form>
