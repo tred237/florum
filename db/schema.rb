64aa1a6add677bf8b6aa8f443db83f5f421b6508
trevor dewindt <trevor.dewindt@gmail.com>
2023-04-12 18:21:46 -0600

    add email column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_040423) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_001620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_040423) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
