a503df7fb4f03ad5477c1776efadc4f4feda9245
trevor dewindt <trevor.dewindt@gmail.com>
2023-04-18 16:45:46 -0600

    create show plant route

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,7 +1,9 @@
 class PlantsController < ApplicationController
     before_action :authorize
+    skip_before_action :authorize, only: [:show]
 
     rescue_from ActiveRecord::RecordInvalid, with: :plant_record_invalid_response
+    rescue_from ActiveRecord::RecordNotFound, with: :plant_record_not_found_repsonse
 
     def index
         plants = Plant.all
@@ -13,6 +15,11 @@ class PlantsController < ApplicationController
         render json: plant, status: :created
     end
 
+    def show
+        plant = Plant.find(params[:id])
+        render json: plant, status: :ok
+    end
+
     private
 
     def plant_params
@@ -21,5 +28,9 @@ class PlantsController < ApplicationController
 
     def plant_record_invalid_response(invalid)
         render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+    def plant_record_not_found_repsonse(invalid)
+        render json: {error: "Plant not found"}, status: :not_found
     end
 end
