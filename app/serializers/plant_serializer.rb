9735090431ec5189c48f32d4b660bf3d1023c1fc
trevor dewindt <trevor.dewindt@gmail.com>
2023-04-20 21:18:37 -0600

    order forum entries in plants serializer

--- app/serializers/plant_serializer.rb
+++ app/serializers/plant_serializer.rb
@@ -1,6 +1,8 @@
 class PlantSerializer < ActiveModel::Serializer
   attributes :id, :name, :image, :light, :water, :size, :description, :safe_for_pets
 
-  has_many :forum_entries
+  has_many :forum_entries do
+    object.forum_entries.order(created_at: :desc)
+  end
   belongs_to :owner, foreign_key: :owner_id, class_name: 'User'
 end
