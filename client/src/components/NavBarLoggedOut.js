6e620d0457836cda60b9749419d25fd398be047a
trevor dewindt <trevor.dewindt@gmail.com>
2023-04-24 22:06:08 -0600

    add florum logo to navbar whne logged out

--- client/src/components/NavBarLoggedOut.js
+++ client/src/components/NavBarLoggedOut.js
@@ -4,6 +4,8 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 
+import florumlogo from "../img/florumlogo.png";
+
 export default function NavBarLoggedOut() {
     const [isLogin, setIsLogin] = useState(true)
 
@@ -11,7 +13,10 @@ export default function NavBarLoggedOut() {
         <div>
                 <Navbar bg="dark" variant="dark">
                     <Container>
-                    <Navbar.Brand>Florum</Navbar.Brand>
+                    <Navbar.Brand>
+                        <img alt="" src={florumlogo} width="30" height="30" className="d-inline-block align-top"/>{' '}
+                        Florum
+                    </Navbar.Brand>
                     <Nav>
                         {isLogin ? <Nav.Link as={Link} to="/signup" onClick={() => setIsLogin(!isLogin)}>Signup</Nav.Link> 
                                  : <Nav.Link as={Link} to="/login" onClick={() => setIsLogin(!isLogin)}>Login</Nav.Link>}
