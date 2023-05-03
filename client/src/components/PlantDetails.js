186af2b15b4f2b10c64480a2e86fd86ff3750d67
trevor dewindt <trevor.dewindt@gmail.com>
2023-05-02 23:40:36 -0600

    add styling and remove user context to plant details

--- client/src/components/PlantDetails.js
+++ client/src/components/PlantDetails.js
@@ -1,16 +1,12 @@
-import { useContext } from 'react';
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image'
 
-import { UserContext } from '../context/User';
 import PlantInformation from './PlantInformation';
 import PlantForum from './PlantForum';
 import Loading from "./Loading";
 import florumlogo from "../img/florumlogo.png";
 
 export default function PlantDetails({ plant, setPlant }) {
-    const { user } = useContext(UserContext)
-
     const handleForumEntrySubmit = (entry) => {
         const updatedPlant = {...plant}
         updatedPlant.forum_entries = [entry, ...updatedPlant.forum_entries]
@@ -19,15 +15,17 @@ export default function PlantDetails({ plant, setPlant }) {
 
     if(!plant.name) <Loading />
     else return (
-        <Container className='w-85'>
+        <Container>
             <Container className='d-flex justify-content-center'>
                 <Image className="w-35 h-25" src={florumlogo} />
             </Container>
             <Container className='d-flex justify-content-center'>
                 <PlantInformation plant={plant} setPlant={setPlant} />
             </Container>
-            <h2>Description</h2>
+            <h2>{plant.description ? "Description" : null}</h2>
             {plant.description}
+            <hr className="hr" />
+            <h2>Comments</h2>
             {plant.forum_entries ? <PlantForum plant={plant} onForumEntrySubmit={handleForumEntrySubmit} />  : null}
         </Container>
     )
