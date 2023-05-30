f8e89261012e9d0bd64700b4fe4d58eed86a08f5
trevor dewindt <trevor.dewindt@gmail.com>
2023-05-30 08:34:08 -0600

    update commented plants on plant edit

--- client/src/components/EditPlantForm.js
+++ client/src/components/EditPlantForm.js
@@ -46,8 +46,9 @@ export default function EditPlantForm({ onCloseModal }) {
                 if(r.ok) r.json().then((updatedPlant) => {
                     const updatedPlants = plants.filter(e => e.id !== updatedPlant.id)
                     const updatedUser = {...user}
-                    const filteredOwnedPlants = updatedUser.owned_plants.filter(e => e.id !== updatedPlant.id)
-                    updatedUser.owned_plants = sortArray([updatedPlant, ...filteredOwnedPlants], false, 'name')
+                    const filteredUserPlants = updatedUser.owned_plants.filter(e => e.id !== updatedPlant.id)
+                    updatedUser.owned_plants = sortArray([updatedPlant, ...filteredUserPlants], false, 'name')
+                    updatedUser.commented_plants = sortArray([updatedPlant, ...filteredUserPlants], false, 'name')
                     setUser(updatedUser)
                     setPlants([...updatedPlants, updatedPlant])
                     setPlant(updatedPlant)
